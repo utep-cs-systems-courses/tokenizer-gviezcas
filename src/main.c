b9e78f1e0ae2457195aa0db7d3c5aa6220b82365
gviezcas <gviezcas@miners.utep.edu>
2021-10-03 12:42:14 -0600

    Added comments

--- src/main.c
+++ src/main.c
@@ -10,19 +10,18 @@ int main()
   char *userInput = malloc(MAX_SIZE * sizeof(char));
   char **tokenized_string = malloc(MAX_SIZE * sizeof(char*));
   char **beginning = malloc(MAX_SIZE * sizeof(char*));
-  beginning = tokenized_string;
   while(*userInput != 'q')
     {
 	  printf("Please enter the item to tokenize:(Enter q to exit)\n");
 	  printf("$ ");
 	  fgets(userInput, MAX_SIZE, stdin);
 	  printf("\n");
-	  beginning = tokenized_string;
-	  tokenized_string = tokenize(userInput);
-	  print_tokens(tokenized_string);
+	  beginning = tokenized_string;  //Save the beginning of the string.
+	  tokenized_string = tokenize(userInput);  //Tokenize the string.
+	  print_tokens(tokenized_string);  //Print the tokens.
 	  printf("\n");
-	  tokenized_string = beginning;
-	  //free_tokens(tokenized_string);
+	  tokenized_string = beginning;  //Get back to the beginning of the string.
+	  //free_tokens(tokenized_string);  //Free the tokens.(Not working)
     }
   printf("Goodbye.\n");
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*If char is a space return true.*/
 int space_char(char c)
 {
   if(c == ' ' || c == '\t' || c == '\n')
@@ -9,7 +9,7 @@ int space_char(char c)
     }
   return 0;
 }
-
+/*If char is not a space return true.*/
 int non_space_char(char c)
 {
   if(!space_char(c))
@@ -18,7 +18,7 @@ int non_space_char(char c)
     }
   return 0;
 }
-
+/*Iterate through string until the first char.*/
 char *word_start(char *c)
 {
   while(*c != '\0')
@@ -34,7 +34,7 @@ char *word_start(char *c)
     }
   return c;
 }
-
+/*Iterate through string until the first space.*/
 char *word_end(char *c)
 {
   while(*c != '\0')
@@ -50,7 +50,7 @@ char *word_end(char *c)
     }
   return c;
 }
-
+/*Iterate through string and count words.*/
 int count_words(char *c)
 {
   int counter = 0;
@@ -69,7 +69,7 @@ int count_words(char *c)
     }
   return counter;
 }
-
+/*Make a copy of the string.*/
 char *copy_str(char *inStr, short len)
 {
   char *copy = malloc(len * sizeof(char)+1);
@@ -82,24 +82,24 @@ char *copy_str(char *inStr, short len)
   copy[i] = '\0';
   return copy;
 }
-
+/*Count the number of letters in a word.*/
 int count_letters(char* str)
 {
   int counter = 0;
   str = word_start(str);
 
-  while(non_space_char(*str))
+  while(non_space_char(*str))  //Iterate through all non space characters
     {
-      counter++;
-      str++;
+      counter++;               //Add to the counter.
+      str++;                   //Move the pointer over.
     }
   return counter;
 }
-
+/*Print the tokens in this format [1]Token.*/
 void print_tokens(char **tokens)
 {
   int i = 1;
-  char **temp = tokens;
+  char **temp = tokens;        //Temp pointer to modify.
   
   while(*temp != 0)
     {
@@ -109,10 +109,10 @@ void print_tokens(char **tokens)
       i++;
     }
 }
-
+/*Free the allocated memory being used.*/
 void free_tokens(char **tokens)
 {
-  char **temp = tokens;
+  char **temp = tokens;     //Temp pointer to modify.
   int i = 0;
   while(*temp != 0)
     {
@@ -125,19 +125,17 @@ void free_tokens(char **tokens)
 char **tokenize(char* str)
 {
   char **tokenized_string = malloc((count_words(str)+1) * sizeof(char));
-  int i = 1;
   char **beginning = malloc((count_words(str)+1) * sizeof(char));
-  beginning = tokenized_string;
-  while(*str != '\0')
+  beginning = tokenized_string;  //Save the beginning of the string.
+  while(*str != '\0') //Iterate through string.
     {
-      str = word_start(str);
-      *tokenized_string = copy_str(str, count_letters(str));
-      str = word_end(str);
-      tokenized_string++;
-      str = word_start(str);
-      i++;
+      str = word_start(str);  //Get to the start of the first word in string.
+      *tokenized_string = copy_str(str, count_letters(str));  //Copy the string.
+      str = word_end(str);  //Get to the end of the first word in sting.
+      tokenized_string++;  //Move the array of string pointer over 4 bytes.
+      str = word_start(str);  //Get to start of the next word.
     }
-  *tokenized_string = 0;
-  tokenized_string = beginning;
-  return tokenized_string;
+  *tokenized_string = 0;  //Add terminator to end of array of strings.
+  tokenized_string = beginning;  //Get to the start of the array of strings.
+  return tokenized_string;  //Return the pointer to the array of strings.
 }
